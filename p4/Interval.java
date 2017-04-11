a09595d69df6d3d6975f95610bf3ab2c510bcb70
aquick2 <aquick2@wisc.edu>
2017-04-11 15:26:35 -0500

    Update Interval.java

--- p4/Interval.java
+++ p4/Interval.java
@@ -1,9 +1,34 @@
-
+/////////////////////////////////////////////////////////////////////////////
+// Semester:         CS367 Spring 2017
+// PROJECT:          Program 4
+// FILE:             Interval.java
+//
+// TEAM:    Team 35a
+// Authors: Jon Sharp, Lindsey Bohr, Allison Quick
+// Author1: Jon Sharp, jsharp4@wisc.edu, jsharp4, 001
+// Author2: Lindsey Bohr, bohr@wisc.edu, bohr, 001
+// Author 3: Allison Quick, aquick2@wisc.edu, aquick2, 001
+//////////////////////////// 80 columns wide //////////////////////////////////
+/**
+ * This class extends Comparable and implements IntervalADT. It has the methods
+ * getStart, getEnd, getLabel, overlaps, contains, and compareTo. 
+ *
+ * <p>Bugs: None that we are aware
+ *
+ * @author Sharp, Bohr, Quick
+ */
 public class Interval<T extends Comparable<T>> implements IntervalADT<T> {
 	private T start;
 	private T end;
 	private String label;
-
+ /**
+ * This constructor assigns the start, end, and label based on what is being
+ *passed in. 
+ *
+ * @param T start (Describe the first parameter here)
+ * @param T end (Do the same for each additional parameter)
+ * @param String label
+ */
     public Interval(T start, T end, String label) {
         this.start = start;
         this.end = end;
